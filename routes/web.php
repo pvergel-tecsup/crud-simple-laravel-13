43ae2de608e45c7485e96a6f0e9ff4290231dca5
Paul Vergel <pvergel@tecsup.edu.pe>
2026-06-11 00:13:07 -0500

    Rutas agregadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::resource('/productos', ProductoController::class);
